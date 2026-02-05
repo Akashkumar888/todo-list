de3684f74e429558eb608e1380ba26924cef0ed9
Akash kumar <akashgkr12@gmail.com>
2026-02-05 23:56:54 +0530

    fix error

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,12 @@ import taskRouter from "./routes/task.route.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://todo-list-2rgz.vercel.app",
-      "https://todo-list-phi-five-15.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
   res.send("API is working");
