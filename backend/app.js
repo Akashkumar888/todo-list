ff1ccc9f121ff0f8648f4e41a70181968e4516f0
Akash kumar <akashgkr12@gmail.com>
2026-02-06 00:12:20 +0530

    fix it

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 import express from "express";
+import cors from "cors";
 import taskRouter from "./routes/task.route.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://todo-list-2rgz-424w9sron-akashs-projects-f5c26188.vercel.app",
+      "https://todo-list-2rgz.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json());
 
